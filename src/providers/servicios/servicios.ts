97bd41e1a1c86fd22af8c5d33df35d50c8b4b00c
Leo1305Pineda <leo1305pineda@gmail.com>
2018-04-25 23:03:24 -0400

    Agregando url heroku

--- src/providers/servicios/servicios.ts
+++ src/providers/servicios/servicios.ts
@@ -3,24 +3,18 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ServiciosProvider {
 
-	public url:string = 'http://localhost:5000/servicios';
+	//public url:string = 'http://localhost:5000';
+  public url:string = 'http://api-sascha.herokuapp.com';
 
   constructor(public http: HttpClient) {
     console.log('Hello ServiciosProvider Provider');
   }
 
   getServicios(){
-  	let miheaders = new HttpHeaders();
-    	miheaders.append('Access-Control-Allow-Origin', '*');
-      	miheaders.append('Content-Type', 'application/json');
-      	miheaders.append('Accept', 'application/json');
-      	miheaders.append('Access-Control-Allow-Headers', 'Origin, Accept, Authorization, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
-  	const httpOptions = {
-    	headers: miheaders
-  	};
-
-  	return this.http.get('http://localhost:5000/servicios'); //, httpOptions
+  	return this.http.get(this.url + '/servicios');
   }
 
-
+  getPromociones(){
+    return this.http.get(this.url + '/promociones');
+  }
 }
